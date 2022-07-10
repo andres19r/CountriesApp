d96f0cca9738a103a54ea9e1dc75490900d9d792
Andres Rivero <andres19rivero@gmail.com>
2022-07-10 17:24:15 -0400

    added suggestions

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -5,36 +5,45 @@ import { CountryService } from '../../services/country.service';
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
-  styleUrls: ['./by-country.component.css']
+  styleUrls: ['./by-country.component.css'],
 })
 export class ByCountryComponent implements OnInit {
-  term: string = ''
-  thereIsError: boolean = false
-  countries: Country[] = []
+  term: string = '';
+  thereIsError: boolean = false;
+  countries: Country[] = [];
+  suggestedCountries: Country[] = [];
+  showSuggestions: boolean = false;
 
-  constructor(private countryService: CountryService) { }
+  constructor(private countryService: CountryService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   search(term: string): void {
-    this.thereIsError = false
-    this.term = term
-    this.countryService.searchCountry(this.term)
-      .subscribe({
-        next: countries => {
-          this.countries = countries
-        },
-        error: err => {
-          this.thereIsError = true
-          this.countries = []
-        }
-      })
+    this.showSuggestions = false;
+    this.thereIsError = false;
+    this.term = term;
+    this.countryService.searchCountry(this.term).subscribe({
+      next: (countries) => {
+        this.countries = countries;
+      },
+      error: (err) => {
+        this.thereIsError = true;
+        this.countries = [];
+      },
+    });
   }
 
   suggestions(term: string) {
-    this.thereIsError = false
-    // TODO: create suggestions
+    this.showSuggestions = true;
+    this.thereIsError = false;
+    this.term = term;
+    this.countryService.searchCountry(term).subscribe({
+      next: (countries) => (this.suggestedCountries = countries.splice(0.5)),
+      error: (err) => (this.suggestedCountries = []),
+    });
   }
 
+  searchSuggested(term: string) {
+    this.search(term);
+  }
 }
