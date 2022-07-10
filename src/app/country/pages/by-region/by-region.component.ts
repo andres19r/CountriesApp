e672d40758fc1bc60e47e2bf5594e6ffe9c193ed
Andres Rivero <andres19rivero@gmail.com>
2022-07-10 15:22:19 -0400

    search by region page

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./by-region.component.css']
 })
 export class ByRegionComponent implements OnInit {
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
+  activeRegion: string = ''
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  activateRegion(region: string) {
+    this.activeRegion = region
+    // TODO: make the call to the service
   }
 
 }
