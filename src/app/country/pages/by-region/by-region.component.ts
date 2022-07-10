306998897a3a94254da6807dc982e45ec0c91caa
Andres Rivero <andres19rivero@gmail.com>
2022-07-10 15:53:03 -0400

    show countries by region

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
-  styleUrls: ['./by-region.component.css']
+  styleUrls: ['./by-region.component.css'],
 })
 export class ByRegionComponent implements OnInit {
-  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
-  activeRegion: string = ''
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion: string = '';
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private countryService: CountryService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   activateRegion(region: string) {
-    this.activeRegion = region
-    // TODO: make the call to the service
+    if (region === this.activeRegion) return;
+    this.activeRegion = region;
+    this.countries = [];
+    this.countryService
+      .searchRegion(region)
+      .subscribe((countries) => (this.countries = countries));
   }
 
+  getClassCSS(region: string): string {
+    return region === this.activeRegion
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
 }
